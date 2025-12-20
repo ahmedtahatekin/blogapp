d7ac07f9651f984b265de679a729149a7a711418
ahmedtahatekin <tekinahmedtaha456@gmail.com>
2025-12-20 09:17:56 +0300

    blogapp: Blog modelinin findBlogById(), createNewBlog(), editSelectedBlog ve deleteSelectedBlog() fonksiyonları yazıldı

--- models/Blog.php
+++ models/Blog.php
@@ -14,12 +14,62 @@ class Blog {
         $this->content = $content;
     }
 
-    function save() {}
+    public static function createNewBlog(int $user_id, string $title, string $content): void {
+        //veritabanına parametrelerle blogu kaydet
+        $stmt = $conn->prepare("INSERT INTO blogs (user_id, title, content) VALUES (?, ?, ?)");
+        $stmt->execute([
+            $user_id,
+            $title,
+            $content
+        ]);
 
-    function getBlog(int $id) {}
+        $stmt = $conn->prepare("SELECT title FROM blogs WHERE title = $title");
+        $stmt->execute();
 
-    function edit(int $id) {}
+        echo "Yeni Blog Başarıyla oluşturuldu!: $stmt";
+    }
 
-    function delete(int $id) {}
+    public static function findBlogById(int $id): ?Blog {
+        //veritabanından blogu çek
+        $stmt = $conn->prepare("SELECT * FROM blogs WHERE id = :id");
+        $stmt->execute([
+            "id" => $id
+        ]);
 
+        //blog datasını al
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //data boşsa null döndür
+        if (!$data) {
+            return null;
+        }
+
+        //yeni blog nesnesi oluştur
+        $blog = new Blog(
+            $data['user_id'],
+            $data['title'],
+            $data['content']
+        );
+
+        //oluşturulan blog nesnesini döndür
+        return $blog;
+    }
+
+    public function editSelectedBlog(int $id, string $title = "", string $content = ""): void {
+        //başlık veya içerik girilmediyse bunları güncelleme
+        if ($title === "") {
+            //veri tabanındaki blog ögesini bul
+            $stmt = $conn->prepare("UPDATE blogs SET content = $content WHERE id = $id");
+        } elseif ($content === "") {
+            //veri tabanındaki blog ögesini bul
+            $stmt = $conn->prepare("UPDATE blogs SET title = $title WHERE id = $id");
+        } elseif (!$title === "" && !$content === "") {
+            //veri tabanındaki blog ögesini bul
+            $stmt = $conn->prepare("UPDATE blogs SET title = $title, content = $content WHERE id = $id");
+        }
+        $stmt->execute();
+    }
+
+    function deleteSelectedBlog(int $id): void {
+    }
 }
