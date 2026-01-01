88ab7857f46460edc7947ef078ec7e69319e4a72
ahmedtahatekin <tekinahmedtaha456@gmail.com>
2026-01-01 12:26:24 +0300

    hata ayıklama modu .envde tutulan APP_ENV değişkenine göre ayarlanacak şekilde bootstrap.php düzenlendi.

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,7 +1,12 @@
 <?php
-// tüm dosyalar için aynı başlatıcı
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+if ($_ENV['APP_ENV'] === 'production') {
+    ini_set('display_errors', 0);
+    ini_set('log_errors', 1);
+    ini_set('error_log', __DIR__ . '/../logs/php_error.log');
+} elseif ($_ENV['APP_ENV'] === 'development') {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
 
 // session
 if (session_status() === PHP_SESSION_NONE) {
